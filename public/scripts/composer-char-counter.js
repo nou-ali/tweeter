7d85ca4f119cf05b0a46738f7a1c1f357561d0dd
vagrant <noura4403@gmail.com>
2021-12-14 19:47:54 +0000

    character count is working

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+  $('#tweet-text').on('keyup', () => {
+    $('.counter').text(140 - $('#tweet-text').val().length);
+    if (($('.counter').text()) < 0) {
+      $('.counter').addClass('overCharacterLimit');
+    } else {
+      $('.counter').removeClass('overCharacterLimit');
+    }
+  });
+});
+
+
+
+
+
+
