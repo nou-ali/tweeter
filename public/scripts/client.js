004b2bdefe08171a825d97b961c4feae63fb744b
vagrant <noura4403@gmail.com>
2021-12-15 21:51:23 +0000

    finally fixed rendering.. tweets are now dynamic!

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,27 +1,74 @@
 // Test / driver code (temporary). Eventually will get this from the server.
-// $(() => {
-
-// }); 
-
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
+$(document).ready(function () {
+const tweetData = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
       "handle": "@SirIsaac"
     },
-  "content": {
+    "content": {
       "text": "If I have seen further it is by standing on the shoulders of giants"
     },
-  "created_at": 1461116232227
-}
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
 
-// const createTweetElement = function(tweet) {
-//will put tweet container here
-// }
+const renderTweets = function(tweets) {
+  // $('#tweetContainer').empty();
 
+  for (const tweet of tweets) {
+    const $htmlTweetsContainer = createTweetElement(tweet);
+    $('#tweetContainer').prepend($htmlTweetsContainer);
+  }
+};
 
-const $tweet = createTweetElement(tweetData);
+const createTweetElement = function(tweet) {
+ const $htmlTweetsContainer = $(`
+ <section class="tweetContainer">
+ <article>
+     <header class="avatar">
+       <div class="avatarProfile">
+         <!-- <i class="fas fa-user-ninja"></i> -->
+         <img class="avatarImage" src=${tweet.user.avatars} alt="">
+         <span class="avatarName">${tweet.user.name}</span>
+       </div>
+       <span>${tweet.user.handle}</span>
+     </header>
+     <div class= "lineForBox">
+       <p class="textInContainer">${tweet.content.text}</p>
+     </div>
+     <footer class="bottomComponents">
+       <span class= "daysCounter">${timeago.format(tweet.created_at)}</span>
+       <div class="icons">
+         <i class="fas fa-heart"></i>
+         <i class="fas fa-retweet"></i>
+         <i class="fas fa-flag"></i>
+       </div>
+     </footer>
+ </article> 
+ </section> `)
+  return $htmlTweetsContainer;
+};
 
+// let $tweet = createTweetElement(tweetData);
 // Test / driver code (temporary)
-// console.log($tweet); // to see what it looks like
-$('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+//console.log($tweet); // 
+// $('.tweetContainer').append($tweet); 
+
+renderTweets(tweetData);
+
+});
+
+
