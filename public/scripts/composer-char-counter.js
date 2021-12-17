b4ec980e516e71ccddcbaaf32f51c277da72fc53
vagrant <noura4403@gmail.com>
2021-12-17 22:14:44 +0000

    minor adjustments to scripts

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,12 +1,20 @@
 $(document).ready(function() {
+  
   $('#tweet-text').on('keyup', () => {
+
     $('.counter').text(140 - $('#tweet-text').val().length);
+
     if (($('.counter').text()) < 0) {
+
       $('.counter').addClass('overCharacterLimit');
+
     } else {
+
       $('.counter').removeClass('overCharacterLimit');
     }
+
   });
+  
 });
 
 
